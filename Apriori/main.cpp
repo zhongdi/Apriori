0a691879c1b5c2e4efe6be27285fddcf98be0469
悟迪 <zhongdi.zd@alibaba-inc.com>
2020-06-27 21:07:08 +0800

    增加变量的初始化

--- Apriori/main.cpp
+++ Apriori/main.cpp
@@ -1,6 +1,8 @@
-#include<stdio.h>
-int main()
-{
-	printf("test   zhonglongbin....");
-	return 0;
+#include <stdio.h>
+int main() {
+  printf("test   zhonglongbin....");
+  int x = 1;
+  int y = 2;
+
+  return 0;
 }
